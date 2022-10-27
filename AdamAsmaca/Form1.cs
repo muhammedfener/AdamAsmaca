d64355c09bb86d8ec96b7bcee7132a3e83a2b2d2
Muhammed Fener <28063634+muhammedfener@users.noreply.github.com>
2022-10-27 10:59:34 +0300

    Add project files.

---
+++ AdamAsmaca/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdamAsmaca
+{
+    public partial class Form1 : Form
+    {
+        List<string> kelimeler = new List<string>() { "kalem", "fare", "çanta" };
+        List<PictureBox> resimler;
+        Oyun oyun;
+        int hak = 0;
+        int bilinenharf = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resimler = new List<PictureBox> { pb1, pb2, pb3, pb4, pb5, pb6 };
+
+            foreach(PictureBox p in resimler)
+            {
+                p.Visible = false;
+            }
+
+            oyun = new Oyun(kelimeler);
+        }
+
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            oyun.OyunBaslat();
+            btnBasla.Enabled = false;
+            foreach (Label label in oyun.LabelListesi)
+            {
+                this.Controls.Add(label);
+            }
+        }
+
+        private void tiklananHarf(object sender, EventArgs e)
+        {
+            Button harf = (Button) sender;
+            int i = oyun.HarfGir(Convert.ToChar(harf.Text.ToLower()));
+            harf.Enabled = false;
+            if (i == -1)
+            {
+                resimler[hak].Visible = true;
+                hak++;
+                if(hak == 6)
+                {
+                    MessageBox.Show("Oyun Bitti! Kaybettiniz!");
+                }
+            }
+            else if (i == -100)
+            {
+                MessageBox.Show("Oyun Bitti! Kaybettiniz!");
+            }
+            else
+            {
+                oyun.LabelListesi[i].Text = harf.Text;
+                bilinenharf++;
+                if(bilinenharf == oyun.SecilenKelime.Length)
+                {
+                    MessageBox.Show("Oyunu Kazandınız!");
+                    btnBasla.Enabled = true;
+                }
+            }
+        }
+    }
+}
---
+++ AdamAsmaca/Oyun.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+namespace AdamAsmaca
+{
+    public class Oyun
+    {
+
+        private List<string> Kelimeler = new List<string>();
+        private string _secilenKelime;
+        private List<Label> _labelListesi = new List<Label>();
+        private int _kalanHak = 6;
+
+        public List<string> KelimeListesi { get => Kelimeler; set => Kelimeler = value; }
+        public string SecilenKelime { get => _secilenKelime; set => _secilenKelime = value; }
+        public List<Label> LabelListesi { get => _labelListesi; set => _labelListesi = value; }
+        public int KalanHak { get => _kalanHak; set => _kalanHak = value; }
+
+        public Oyun(List<string> kelimeler)
+        {
+            Kelimeler = kelimeler;
+        }
+
+        public void OyunBaslat(int i=0)
+        {
+            Random rnd = new Random();
+            if(i != 0)
+                SecilenKelime = Kelimeler[rnd.Next(i,Kelimeler.Count)];
+            else
+                SecilenKelime = Kelimeler[rnd.Next(Kelimeler.Count)];
+
+            LabelOlustur(SecilenKelime.Length);
+        }
+
+        private void LabelOlustur(int labelsayisi)
+        {
+            for (int a = 0; a < labelsayisi; a++)
+            {
+                _labelListesi.Add(new Label());
+            }
+            int ilknokta = 470;
+            foreach(Label label in _labelListesi)
+            {
+                label.Location = new Point(ilknokta, 50);
+                label.Size = new Size(25, 25);
+                ilknokta += 30;
+                label.Visible = true;
+                label.Text = "_";
+                label.Font = new Font("Arial", 16);
+            }
+        }
+
+        public int HarfGir(char harf)
+        {
+            if(KalanHak == 0)
+            {
+                return -100;
+            }
+            if (SecilenKelime.Contains(harf))
+            {
+                return SecilenKelime.IndexOf(harf);
+            }
+            else
+            {
+                KalanHak--;
+                return -1;
+            }
+        }
+    }
+}
